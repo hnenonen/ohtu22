823126069ea6dcf2ddc8873e7690869442f575d2
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-11-14 20:56:59 +0200

    week2, exe 10-12, all done now

--- viikko2/verkkokauppa-1/src/kauppa.py
+++ viikko2/verkkokauppa-1/src/kauppa.py
@@ -1,14 +1,14 @@
-from varasto import Varasto
-from pankki import Pankki
+from varasto import varasto_olio
+from pankki import pankki_olio
 from ostoskori import Ostoskori
-from viitegeneraattori import Viitegeneraattori
+from viitegeneraattori import viite_olio
 
 
 class Kauppa:
-    def __init__(self):
-        self._varasto = Varasto.get_instance()
-        self._pankki = Pankki.get_instance()
-        self._viitegeneraattori = Viitegeneraattori.get_instance()
+    def __init__(self, varasto = varasto_olio, pankki = pankki_olio, viite = viite_olio):
+        self._varasto = varasto
+        self._pankki = pankki
+        self._viitegeneraattori = viite
         self._kaupan_tili = "33333-44455"
 
     def aloita_asiointi(self):
