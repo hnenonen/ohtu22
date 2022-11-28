74feaa7079ae80d417d176703f789e8d17b13cb2
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-11-28 20:18:38 +0200

    w4, exe5.2 ostoskoriin voi laittaa tavaroita ja se pitää kirjaa lkm

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,13 +3,15 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        self.sisalto = {}
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         amount = 0
+        for item in self.sisalto.values():
+            amount += item.lukumaara()    
         return amount
-        pass
+        
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -20,7 +22,11 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        pass
+        if lisattava.nimi in self.sisalto:
+            self.sisalto[lisattava.nimi].muuta_lukumaaraa(1)
+        else:
+            self.sisalto[lisattava.nimi] = Ostos(lisattava)
+
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
