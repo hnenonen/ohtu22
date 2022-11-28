5e277503243237e894d5927af542b3cfb53b22e8
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-11-28 20:34:24 +0200

    w4 exe5.5-6 samojen tuotteiden laskuri ja hinta toimii korissa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -34,3 +34,16 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(jaffa)
         self.assertEqual(self.kori.hinta(), 8)
 
+    
+    def test_saman_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tuotetta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)        
+        self.kori.lisaa_tuote(maito)   
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_saman_kahden_tuotteen_lisaamisen_jalkeen_korin_hinta_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)        
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
+
