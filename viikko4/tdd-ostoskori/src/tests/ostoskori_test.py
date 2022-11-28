742c1bc46288575fd437dcffdec4dcf856ff1aef
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-11-28 20:29:40 +0200

    w4 exe5.4 useamman tuotteen hinnat toimii

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -25,3 +25,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)        
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korin_hinta_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)        
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
